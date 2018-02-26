c443d91b0a6476119a0e6e74467be99c4a40ecab
Ira Chan <irachan@buffalo.edu>
2018-02-26 18:58:44 -0500

    added registration page
    added new scene and some assets

--- Another.World/Assets/scripts/goToScene.cs
+++ Another.World/Assets/scripts/goToScene.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class goToScene : MonoBehaviour {
+public class goToScene : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	public void switchScene(int i)
-	{
-		SceneManager.LoadScene(i);
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void switchScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
 }
