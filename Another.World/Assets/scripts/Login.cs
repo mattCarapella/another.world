074a95465846b49221dfbafa56d2a7ab14dfddc8
hel1ow0r1d <35990813+hel1ow0r1d@users.noreply.github.com>
2018-02-26 20:36:12 -0500

    Back-end Integration Complete
    Front-end system is now properly working with Back-end system. Need to integrate the Login and Registration form with the buttons. Successful message pops up in the console window when the Return key is pressed.

---
+++ Another.World/Assets/scripts/Login.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class Login : MonoBehaviour {
+
+    public GameObject email;
+    public GameObject password;
+
+    private string Email;
+    private string Password;
+
+    string LoginURL = "http://anotherworld.atwebpages.com/Login.php";
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (email.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return)) StartCoroutine (LoginToDB(Email, Password));
+
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+    }
+
+    IEnumerator LoginToDB(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("emailPost", Email);
+        form.AddField("passwordPost", Password);
+
+        WWW www = new WWW(LoginURL, form);
+        yield return www;
+        Debug.Log(www.text);
+    }
+}
---
+++ Another.World/Assets/scripts/Registration.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class Registration : MonoBehaviour {
+
+    public GameObject username;
+    public GameObject email;
+    public GameObject password;
+    public GameObject confirm_password;
+
+    private string Username;
+    private string Email;
+    private string Password;
+    private string Confirm_Password;
+    
+    string CreateUserURL = "http://anotherworld.atwebpages.com/Register.php";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+    public void Register(string username, string email, string password, string confirm_password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("emailPost", email);
+        form.AddField("passwordPost", password);
+
+        WWW www = new WWW(CreateUserURL, form);
+        Debug.Log(www.text);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                email.GetComponent<InputField>().Select();
+            }
+
+            if (email.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+
+            if (password.GetComponent<InputField>().isFocused)
+            {
+                confirm_password.GetComponent<InputField>().Select();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return)) // || Input.GetMouseButtonDown(0)
+        {
+            if (Password == Confirm_Password)
+            {
+                Register(Username, Email, Password, Confirm_Password);
+            }
+            else
+            {
+                print("Password does not match");
+            }
+        }
+
+        Username = username.GetComponent<InputField>().text;
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        Confirm_Password = confirm_password.GetComponent<InputField>().text;
+
+    }
+}
