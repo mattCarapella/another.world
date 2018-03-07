8476aa505f43c86557791943d2e2ed7503a546d0
Xuanyu Dong <xuanyudo@buffalo.edu>
2018-03-07 15:02:24 -0500

    basic player movement and collider setup

---
+++ Another.World/Assets/scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    protected string _name;
+    
+
+    void loadPlayer()
+    {
+
+    }
+    void loadItems()
+    {
+
+    }
+	// Use this for initialization
+	void Start () {
+        loadPlayer();
+        loadItems();
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Another.World/Assets/scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float _speed;
+    protected Rigidbody _rb;
+    
+    // Use this for initialization
+	void Start () {
+        _speed = 20000f;
+		
+	}
+
+    void Awake() {
+        _rb = GetComponent<Rigidbody>();
+
+    }
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        Debug.Log(x);
+        //transform.Translate(x, 0, 0);
+        //transform.Translate(0, 0, z);
+
+        Vector3 movement = new Vector3(x * Time.deltaTime * _speed, 0, z * Time.deltaTime * _speed);
+        movement = transform.TransformDirection(movement);
+        _rb.velocity = (movement);
+    }
+    
+
+}
