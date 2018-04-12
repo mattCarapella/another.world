b0076f042116fdcb886c171120da4c15376e73de
Ira Chan <irachan@buffalo.edu>
2018-04-12 14:33:32 -0400

    Added instruction page, inventory prefab (add to player), male and female models
    Needs to be added to player loaded in

--- Another.World/Assets/scripts/Player.cs
+++ Another.World/Assets/scripts/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour {
     private gameController _controller;
     private static bool created = false;
     private GameObject _player;
+    public Object[] loadedAssets;
+    bool dd =false;
+    int chosen = 0;
 
     void Awake()
     {
@@ -32,14 +35,86 @@ public class Player : MonoBehaviour {
         inhand.transform.position = this.transform.forward * 5+this.transform.position;
         GameObject.Destroy(sphere);
     }
-    void loadItems()
+    public void chosenOne()
     {
+        chosen = 1;
+    }
+    IEnumerator loadItems()
+    {
+        Debug.Log("********* NOW IN LOADITEMS() *********");
+        string url = "http://ec2-18-232-184-23.compute-1.amazonaws.com/assetbundles/asset_bundle_1";
+        WWW www = WWW.LoadFromCacheOrDownload(url, 1);
+
+        //int itemct = 0;
+
+        //   while (!www.isDone)
+        //   {
+        //     Debug.Log("******** DOWNLOADING ASSETBUNDLE ******");
+
+        //            if (www.assetBundle != null)
+        //          {
+        //            Debug.Log("********* LOADING ASSETS ");
+        //
+        //          AssetBundle bundle = www.assetBundle;
+        //        //				string[] assetList = bundle.GetAllAssetNames();
+        //      Object[] assets = bundle.LoadAllAssets();
+        //assets = bundle.LoadAllAssets();
+        //  loadedAssets = assets;
+        //    foreach (Object asset in assets)
+        //  {
+        //    Debug.Log("******** " + asset.name);
+        //  Instantiate(asset, transform.position, transform.rotation);
+        //}
+
+        //   itemct++;
+        //}
+        //}
+        yield return www;
+        Debug.Log("current asset bundle " + www.assetBundle);
+        if (www.assetBundle != null)
+        {
+            AssetBundle bundle = www.assetBundle;
+            Object[] assets = bundle.LoadAllAssets();
+            loadedAssets = assets;
+            foreach (Object asset in assets)
+            {
+                Debug.Log("******** " + asset.name);
+                //Instantiate(asset, transform.position, transform.rotation);
+            }
+        }
+        Debug.Log("loaded assets " + loadedAssets.Length);
+        // GameObject.Find("Test test").GetComponentInChildren<Text>().text = loadedAssets[0].name;
+        // GameObject.Find("ExitInventoryMenu").GetComponentInChildren<Text>().text = "*******";
+
 
     }
-	// Use this for initialization
-	void Start () {
+    public void loadfromInventory(int assetNum)
+    {
+        GameObject temp = (GameObject)loadedAssets[assetNum];
+        Instantiate(loadedAssets[assetNum], transform.position + new Vector3(0, 0, 5), transform.rotation, inhand.transform);
+        //inhand = (GameObject)loadedAssets[assetNum];
+        Debug.Log(inhand);
+        /*  Mesh mesh = temp.GetComponent<MeshFilter>().sharedMesh;
+          Material material = temp.GetComponent<MeshRenderer>().sharedMaterial;
+          inhand = (GameObject)loadedAssets[assetNum];
+          inhand.GetComponent<MeshFilter>().mesh = mesh;
+          inhand.GetComponent<Renderer>().material = material;
+          inhand.transform.position = this.transform.forward * 5 + this.transform.position;
+          Debug.Log("Item placed: " + loadedAssets[assetNum]);
+          Destroy(temp);*/
+    }
+
+    public void refreshInv()
+    {
+        GameObject.Find("Item 0").GetComponentInChildren<Text>().text = loadedAssets[0].name;
+        GameObject.Find("Item 1").GetComponentInChildren<Text>().text = loadedAssets[1].name;
+        GameObject.Find("Item 2").GetComponentInChildren<Text>().text = loadedAssets[2].name;
+        GameObject.Find("Item 3").GetComponentInChildren<Text>().text = loadedAssets[3].name;
+    }
+    // Use this for initialization
+    void Start () {
         loadPlayer();
-        loadItems();
+        StartCoroutine("loadItems");
         _controller = game.GetComponent<gameController>();
     }
 	// Update is called once per frame
