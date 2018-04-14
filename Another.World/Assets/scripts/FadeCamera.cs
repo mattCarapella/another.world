c1e2249e437ceb021a79e735da9656856a683ca6
joshuaemer <joshuaem@buffalo.edu>
2018-04-14 14:57:57 -0400

    Finished issuse

---
+++ Another.World/Assets/scripts/FadeCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//Fades the camera and rotates it upwards 
+//This script should be attached to the MainCamera
+//Call the Fade function from a different script to begin the fading
+public class FadeCamera : MonoBehaviour {
+    private bool begin = false;
+    private float alphaFadeValue = 0;
+    private Texture2D whiteTexture;
+    private float rotatation = -2;
+    private float start_rot;
+    // Use this for initialization
+    void Start () {
+        whiteTexture = Texture2D.whiteTexture;
+        start_rot = transform.rotation.eulerAngles.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (begin)
+        {
+            
+            //rotatation -= (rotatation*Time.deltaTime);
+            transform.Rotate(10*Vector3.left * Time.deltaTime);
+        }
+	}
+
+    public void Fade() {
+        begin = true;
+        
+    }
+
+
+    void OnGUI()
+    {
+        if (begin)
+        {
+            alphaFadeValue += Mathf.Clamp01(Time.deltaTime / 5);
+
+
+            GUI.color = new Color(1, 1, 1, alphaFadeValue);
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), whiteTexture);
+        }
+
+    }
+
+
+
+}
--- Another.World/Assets/scripts/Login.cs
+++ Another.World/Assets/scripts/Login.cs
@@ -21,6 +21,12 @@ public class Login : MonoBehaviour
     public static string send_email = null;
 
     string URL = "http://ec2-18-232-184-23.compute-1.amazonaws.com/Login.php";
+
+    //The amount of time in seconds that should be waited after a succesful login before switching scence, allows the camera to fade
+    float start_wait_time;
+    private float waitTime = 3;
+    private bool beginWaiting = false;
+
     // Use this for initialization
     void Start()
     {
@@ -42,6 +48,13 @@ public class Login : MonoBehaviour
 
         Email = email.GetComponent<InputField>().text;
         Password = password.GetComponent<InputField>().text;
+
+        if (beginWaiting) {
+            if(Time.time - start_wait_time >= 3)
+            {
+                switchScene(1);
+            }
+        }
     }
 
     public void LoginButton()
@@ -74,11 +87,15 @@ public class Login : MonoBehaviour
 
         if (logc == "IN")
         {
+            
             send_id = int.Parse(infoparts[1]);
             send_username = infoparts[2];
             send_email = infoparts[3];
-
-            switchScene(1);
+            //Tell the camera to start fading
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FadeCamera>().Fade();
+            beginWaiting = true;
+            start_wait_time = Time.time;
+            //switchScene(1);
         }
         else
         {
@@ -91,7 +108,8 @@ public class Login : MonoBehaviour
 
     public void switchScene(int i)
     {
-        SceneManager.LoadScene(i);
+        
+        SceneManager.LoadScene(1);
     }
 
     public void messagebutton()
