48cdb37481a810b7ccd5ff1c02e722ae049f0559
hel1ow0r1d <35990813+hel1ow0r1d@users.noreply.github.com>
2018-05-01 01:17:34 -0400

    Added username capability for Login #74
    User can now login with their username too #74

--- PHP Scripts/Login.php
+++ PHP Scripts/Login.php
@@ -2,35 +2,64 @@
 include ("inc/Credentials.inc");
 error_reporting(0);
 
-$user_email = $_POST[htmlentities("emailPost")];
+$username_email = $_POST[htmlentities("usernameemailPost")];
 $user_password = hash('sha256', $_POST[htmlentities("passwordPost")]);
 
-if ($user_email != "" && $user_password != "") {
+if ($username_email != "" && $user_password != "") {
     
-    $sql = "SELECT id, username, password, fuser, fpass FROM users WHERE email = ?";
-    
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('s', $user_email); // bind parameters for markers
-    $stmt->execute(); // execute query
-    $stmt->bind_result($id, $username, $db_password, $fuser, $fpass); // bind result variable
-    $stmt->store_result(); // fetch value
-    
-    if ($stmt->num_rows == 1) {
-        if ($stmt->fetch()) {
-            if (decrypt($db_password) == $user_password) {
-                echo "IN;".$id.";".$username.";".$user_email.";".$fuser.";".$fpass;
+    if (filter_var($username_email, FILTER_VALIDATE_EMAIL)) {
+        $sql = "SELECT id, username, password, fuser, fpass FROM users WHERE email = ?";
+        
+        if ($stmt = $conn->prepare($sql)) {
+            $stmt->bind_param('s', $username_email); // bind parameters for markers
+            $stmt->execute(); // execute query
+            $stmt->bind_result($id, $username, $db_password, $fuser, $fpass); // bind result variable
+            $stmt->store_result(); // fetch value
+            
+            if ($stmt->num_rows == 1) {
+                if ($stmt->fetch()) {
+                    if (decrypt($db_password) == $user_password) {
+                        echo "IN;" . $id . ";" . $username . ";" . $username_email . ";" . $fuser . ";" . $fpass;
+                    } else {
+                        echo "Wrong Password";
+                    }
+                }
             } else {
-                echo "Wrong Password";
+                echo "User does not exist";
             }
+            $stmt->close();
+        } else {
+            die("Errormessage: " . $conn->error);
+        }
+    } else if (preg_match("/^[a-z0-9]+$/", $username_email)) {
+        $sql = "SELECT id, email, password, fuser, fpass FROM users WHERE username = ?";
+        
+        if ($stmt = $conn->prepare($sql)) {
+            $stmt->bind_param('s', $username_email); // bind parameters for markers
+            $stmt->execute(); // execute query
+            $stmt->bind_result($id, $email, $db_password, $fuser, $fpass); // bind result variable
+            $stmt->store_result(); // fetch value
+            
+            if ($stmt->num_rows == 1) {
+                if ($stmt->fetch()) {
+                    if (decrypt($db_password) == $user_password) {
+                        echo "IN;" . $id . ";" . $username_email . ";" . $email . ";" . $fuser . ";" . $fpass;
+                    } else {
+                        echo "Wrong Password";
+                    }
+                }
+            } else {
+                echo "User does not exist";
+            }
+            $stmt->close();
+        } else {
+            die("Errormessage: " . $conn->error);
         }
     } else {
-        echo "User does not exist";
+        echo "Wrong username/email format, try again";
     }
-    $stmt->close();
-} else {
-    echo "Email and/or Password cannot be empty";
+    $conn->close();
 }
-$conn->close();
 
 function decrypt($password)
 {
