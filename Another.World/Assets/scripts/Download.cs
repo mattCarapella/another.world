3416564192c16683c34f1d62ae42275303545591
hel1ow0r1d <35990813+hel1ow0r1d@users.noreply.github.com>
2018-05-02 23:59:35 -0400

    Added Logout Button #75 and integrated Upload and Download #12
    Added logout button. Upload and download are now working.

---
+++ Another.World/Assets/scripts/Download.cs
@@ -0,0 +1,33 @@
+﻿using SFB;
+using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class Download : MonoBehaviour {
+
+    private int id = Login.send_id;
+    private string username = Login.ftp_user;
+    private string password = Login.ftp_password;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void downloadbutton()
+    {
+        var savepath = StandaloneFileBrowser.OpenFolderPanel("Select the folder where you want to save all of your files ", "", false);
+
+        FTP client = new FTP(@"ftp://18.232.184.23/", username, password);
+        string[] listing = client.directoryListSimple(id + "/");
+        for (int i = 0; i < listing.Count()-1; i++) {
+            client.download(id +"/"+listing[i], savepath[0]+Path.DirectorySeparatorChar+listing[i]);
+        }
+    }
+}
---
+++ Another.World/Assets/scripts/Logout.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logout : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LogoutButton()
+    {
+        Login.send_id = 0;
+        Login.send_username = null;
+        Login.send_email = null;
+        Login.ftp_user = null;
+        Login.ftp_password = null;
+
+        switchScene(0);
+    }
+
+    public void switchScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
